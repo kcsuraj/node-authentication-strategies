0b0dc044018209e0bdefb21f6abb2cef5258df45
Suraj kc <isurajkc@gmail.com>
2020-06-01 18:45:46 +0545

    node and mongo setup in basic auth app

---
+++ basic-auth/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".env" });
+
+console.log(process.env.dbURI);
+
+// Create Express server
+const app = express();
+
+// Connect to MongoDB database
+mongoose
+  .connect(`${process.env.DB_URI}/${process.env.DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(
+      "MongoDB connection error. Please make sure MongoDB is running. " + err
+    );
+  });
+
+app.set("port", process.env.PORT || 5200);
+
+module.exports = app;
